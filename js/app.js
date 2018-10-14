e85721458c6e9588f4ecbb988f4894270079af6e
jose d lopez <jose@devlabs.vc>
2018-10-14 00:41:24 -0300

    limpiar espacios y alieniar antes de corregir

---
+++ js/app.js
@@ -0,0 +1,110 @@
+(function ($) {
+	"use strict";
+
+/*==================================================================
+[ Validate after type ]*/
+$('.validate-input .input100').each(function() {
+	$(this).on('blur', function() {
+		if(validate(this) == false) {
+			showValidate(this);
+		} else {
+			$(this).parent().addClass('true-validate');
+		}
+	});  
+});
+
+/*==================================================================
+[ Validate ]*/
+var input = $('.validate-input .input100');
+
+$('.validate-form').on('submit',function() {
+	var check = true;
+
+	for (var i=0; i<input.length; i++) {
+		if (validate(input[i]) == false) {
+			showValidate(input[i]);
+			check=false;
+		}
+	}
+
+	return check;
+});
+
+
+$('.validate-form .input100').each(function() { 
+	$(this).focus(function() {
+		hideValidate(this);
+		$(this).parent().removeClass('true-validate');
+	});
+});
+
+
+
+})();
+
+//ESTABA en VALIDACION
+
+function validateForm() {
+	if (document.getElementById("ddOne").value == "" || document.getElementById("ddTwo").value == "" || document.getElementById("ddThree").value == "") {
+		alert("You need to fill in all three dropboxes and not just some");
+	return false;
+}
+
+//ESTABA en ACTIVIDADES
+
+/*function createSelect() {
+var myDiv = document.getElementById("myDiv");
+
+  //Create array of options to be added
+  var array = ["Generales - San Francisco", "Generales - Oakland", "Generales - Union City"];
+
+  //Create and append select list
+  var selectList = document.createElement("select");
+  selectList.setAttribute("id", "mySelect");
+  myDiv.appendChild(selectList);
+
+  //Create and append the options
+  for (var i = 0; i < array.length; i++) {
+    var option = document.createElement("option");
+    option.setAttribute("value", array[i]);
+    option.text = array[i];
+    selectList.appendChild(option);
+  }
+}
+document.getElementById("btnTest").addEventListener("click", function(){
+  createSelect();
+});*/
+
+function validationForm() {
+  var buttonSelect = document.getElementById("buttonSelect");
+  var chosenButton = buttonSelect.options[buttonSelect.selectedIndex].value;
+  if (chosenButton == 0) {
+    return false;
+    alert("Por favor, seleccionar una actividad");
+  } else {
+    alert("Seleccionastes : " + chosenButton);
+  return true;
+  }
+};
+
+//Assign function name from html
+//function myFunction() {
+
+//Take the id from html and let it equal to x
+//var x = document.getElementById("myDiv");
+
+//else if statement to hide and show element 
+/*if (x.style.display === "block") {
+    x.style.display = "none";
+} else if (x.style.display === ""){
+  x.style.display = "none";
+}
+  else {
+    x.style.display = "block";
+    }
+  };*/
+
+//
+//var y = document.querySelectorAll('.dropbtn');
+//target.addEventListener()
+
